04031859d00af7c48e9c6981663002d1646645fe
imdong-geun <dg600123@gmail.com>
2022-08-07 16:09:14 +0900

    섬의 개수 문제

---
+++ Algorithm_interview/32_number_of_islands.py
@@ -0,0 +1,44 @@
+# 32) 섬의 개수
+
+from typing import List
+
+
+def numIslands(grid: List[List[str]]) -> int:
+    def dfs(i, j):
+        # 더 이상 땅이 아닌 경우 종료한다.
+        if i < 0 or i >= len(grid) or \
+                j < 0 or j >= len(grid[0]) or \
+                grid[i][j] != '1':
+            return
+
+        grid[i][j] = 0
+        dfs(i + 1, j)
+        dfs(i - 1, j)
+        dfs(i, j + 1)
+        dfs(i, j - 1)
+
+    count = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == '1':
+                dfs(i, j)
+                count += 1
+
+    return count
+
+
+# islands = [
+#     ["1", "1", "1", "1", "0"]
+#     , ["1", "1", "0", "1", "0"]
+#     , ["1", "1", "0", "0", "0"]
+#     , ["0", "0", "0", "0", "0"]
+# ]
+
+islands = [
+    ["1", "1", "0", "0", "0"]
+    , ["1", "1", "0", "0", "0"]
+    , ["0", "0", "1", "0", "0"]
+    , ["0", "0", "0", "1", "1"]
+]
+
+print(numIslands(islands))
